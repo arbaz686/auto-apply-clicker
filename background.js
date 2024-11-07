d1cba118f02a3234793e94d6605b5112c66531b6
arbaz686 <arbazahmad789@gmail.com>
2024-11-07 09:37:03 +0530

    auto-click extension

---
+++ background.js
@@ -0,0 +1,7 @@
+// Send a message every 2 seconds to trigger a click
+setInterval(() => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "clickApply" });
+    });
+  }, 2000);
+  
---
+++ content.js
@@ -0,0 +1,10 @@
+// Function to find and click the Apply button every 2 seconds
+function clickApplyButton() {
+    const applyButton = document.querySelector(".btn.btn-lg.btn-primary.new-btn");
+    if (applyButton) {
+        applyButton.click();
+    }
+}
+
+// Set an interval to click the Apply button every 2 seconds
+setInterval(clickApplyButton, 2000);
